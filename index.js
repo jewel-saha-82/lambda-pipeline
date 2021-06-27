a282138d5e2f9e9f76d24e0079ecd19e40473f9e
Jewel Saha <jewel.saha@cognizant.com>
2021-06-27 12:01:09 +0530

    xzcs

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var time = require('time');
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
-    callback(null, {
-        statusCode: '200',
-        body: 'Dude, The time in Kolkata is: ' + currentTime.toString(),
-    });
+	callback(null, {
+		statusCode: '200',
+		body: 'Dude, this is nodejs.'
+	});
 };
